738278e24469a9576c9b37e188261a6e61221ce4
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2025-10-16 21:30:57 +0700

    Update

--- src/pages/menu-list-page/menu-list-page.jsx
+++ src/pages/menu-list-page/menu-list-page.jsx
@@ -122,7 +122,7 @@ export const MenuListPage = () => {
           {filteredItems.map((item) => (
             <Card
               key={item.id}
-              onClick={() => navigate(`/shops/${shop_id.id}`)}
+              onClick={() => navigate(`/shops/${item.shopId}`)}
               className="overflow-hidden hover:shadow-lg transition-all duration-300 group cursor-pointer"
             >
               <div className="relative">
