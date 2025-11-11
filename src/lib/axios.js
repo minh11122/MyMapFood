340cc54d8dba10a8677a8b06bdb5e09f13572e11
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2025-11-11 14:19:19 +0700

    ca

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const baseURL = 'https://backmymapfood.onrender.com/api/'; 
+const baseURL = 'https://backmymapfood.onrender.com/api/'; 
 
-const baseURL = "http://localhost:9999/api";
+// const baseURL = "http://localhost:9999/api";
 
 const api = axios.create({
   baseURL: baseURL,
